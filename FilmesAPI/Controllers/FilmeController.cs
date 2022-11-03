672946a144c685d5009b1da649c7d2a5d2c77bd0
Humberto Fernandes <humbertofernandes08@hotmail.com>
2022-11-03 16:06:59 -0300

    Criando metodo para buscar filme pelo id

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -9,12 +9,25 @@ namespace FilmesAPI.Controllers
     {
 
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
+            filme.Id = id++;
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> BuscaFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme buscaFilmePorId(int id){
+
+            return filmes.FirstOrDefault(filme => filme.Id == id);
         }
     }
 }
